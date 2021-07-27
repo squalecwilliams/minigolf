648de0f0b6823e10e8652fc49ff1949e2a6d4e84
Alec Williams <alecwilliams@Alecs-MacBook-Pro.local>
2021-07-27 12:47:45 -0500

    third push

--- js/raycasting-test.js
+++ js/raycasting-test.js
@@ -215,7 +215,7 @@ function run(){
                     Engine.clear(engine)
                     if(holeNum > MAX_HOLES){
                         localStorage.setItem('mostRecentScore', strokeCount)
-                        return window.location.assign("file:///Users/alecwilliams/minigolf/end.html")
+                        return window.location.assign("/end.html")
                     }
                     run()
                 }, 1000)
@@ -237,7 +237,7 @@ function run(){
                     Engine.clear(engine)
                     if(holeNum > MAX_HOLES){
                         localStorage.setItem('mostRecentScore', strokeCount)
-                        return window.location.assign("file:///Users/alecwilliams/minigolf/end.html")
+                        return window.location.assign("/end.html")
                     }
                     run()
                 }, 1000)
